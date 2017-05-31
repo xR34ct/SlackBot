0eca1f64a3ab7840e4db103a417da0f25b6c80a0
xR34ct <avdagic94@gmail.com>
2017-05-31 22:58:32 +0200

    Slack bot

---
+++ daemon.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+start () { 
+	#. /home/xr34ct/morpheus/bin/activae
+	/home/xr34ct/morpheus/bin/python /home/xr34ct/Documents/morpheus/morpheus.py > /dev/null 2>&1 &
+}
+
+stop () {
+	pid=$(pgrep -f morpheus)
+	kill "$pid"
+}
+
+case $1 in
+	start|stop) "$1" ;;
+esac
---
+++ morpheus.py
@@ -0,0 +1,68 @@
+import os
+import time
+from slackclient import SlackClient
+from env import *
+
+# starterbot's ID as an environment variable
+#BOT_ID = os.environ.get("BOT_ID")
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+
+
+# instantiate Slack & Twilio clients
+slack_client = SlackClient(SLACK_BOT_TOKEN)
+
+
+def handle_command(command, channel):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+    response = "Not sure what you mean Mortal"
+    if command.startswith('inv'):
+	try:
+	       	with open('/home/xr34ct/Documents/scripts/.inv') as f: s = f.read()
+		response = 'The gods are offering these services:\n\n' + s
+	except OSError:
+		response = 'Nothing here Mortal'
+    if command.startswith('ok'):
+	try:
+		if os.stat('/home/xr34ct/Documents/scripts/.alerts').st_size > 0:
+			with open('/home/xr34ct/Documents/scripts/.alerts') as f: s = f.read()
+			response = 'The following services of the gods are unavailable:\n\n' + s
+		else:
+			response = 'The god are generous Mortal'
+	except OSError:
+		response = 'Nothing here Mortal'
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+
+
+def parse_slack_output(slack_rtm_output):
+    """
+        The Slack Real Time Messaging API is an events firehose.
+        this parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel']
+    return None, None
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("Morpheus connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
